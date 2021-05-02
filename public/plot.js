071ec95609eb3b47ce2cd78274a49735cc14b6bb
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-05-02 23:12:46 +0200

    rm unused graph

--- public/plot.js
+++ public/plot.js
@@ -50,68 +50,8 @@ function plot2() {
             };
             config = {
             };
-            Plotly.newPlot('graph-stamp', [temp], layout, config);
-        });
-    }
-}
-function plot() {
-    if (hasLocalStorageUser == "OK") {
-        // Firestore
-        var db = firebase.firestore();
-        var cur = db.collection('room');
-        cur = cur.orderBy('timestamp', 'desc').limit(2);
-        // variables
-        var x = [];
-        var y = [];
-        var temp;
-        var layout;
-        var config;
-        var dataArray = [];
-
-        function comp(a, b) { // date comparator
-            var Da = new Date(a[0]);
-            var Db = new Date(b[0]);
-            return Da.getTime() - Db.getTime();
-        }
-
-        function load(entry) {
-            x.push(entry[0]);
-            y.push(entry[1]['temperature']);
-        }
-
-        cur.get().then(function (docs) {
-            docs.forEach(function(doc) {
-                var tempArray = Object.entries(doc.data());
-                tempArray = tempArray.filter((value) => {return value[0] != "timestamp"});
-                tempArray.forEach((entry) => {
-                    entry[0] = doc.id + ' ' + entry[0];
-                });
-                dataArray.push(...tempArray);
-            });
-            dataArray.sort(comp);
-            dataArray.forEach(load);
-            temp = {
-                type: "Scattergl",
-                mode: "lines",
-                name: 'teplota',
-                x: x,
-                y: y
-            };
-            layout = {
-                title: 'Teplota',
-                showlegend: true,
-                xaxis: {
-                    title: 'čas'
-                },
-                yaxis: {
-                    title: 'teplota'
-                }
-            };
-            config = {
-            };
             Plotly.newPlot('graph', [temp], layout, config);
         });
     }
 }
-plot();
 plot2();
